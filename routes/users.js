dc33a2b7189054057a0a7c3f59f0e03bf8b3b7c6
Rochette Raphaël <rochette.raph@gmail.com>
2024-03-04 12:58:20 +0100

    Backend SignIn

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-require('../models/connection');
+require("../models/connection");
 
-const User = require('../models/users');
-const { checkBody } = require('../modules/checkBody');
-const uid2 = require('uid2');
-const bcrypt = require('bcrypt');
+const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
 
 // SIGNUP
 
-router.post('/signup', (req, res) => {
-  if (!checkBody(req.body, ['username', 'email', 'password'])) {
-    res.json({ result: false, error: 'Missing or empty fields' });
+router.post("/signup", (req, res) => {
+  if (!checkBody(req.body, ["username", "email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
     return;
   }
 
-// Vérification notation de l'email
+  // Vérification notation de l'email
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(req.body.email)) {
-    res.json({ result: false, error: 'Invalid email format' });
+    res.json({ result: false, error: "Invalid email format" });
     return;
   }
 
   // Regarder si l'utilisateur existe déjà avec un $or qui vérifier si l'un des deux est déjà utilisé
-  User.findOne({ $or: [{ username: req.body.username }, { email: req.body.email }] }).then(data => {
+  User.findOne({
+    $or: [{ username: req.body.username }, { email: req.body.email }],
+  }).then((data) => {
     if (data === null) {
       const hash = bcrypt.hashSync(req.body.password, 10);
 
@@ -35,33 +37,33 @@ router.post('/signup', (req, res) => {
         token: uid2(32),
       });
 
-      newUser.save().then(newDoc => {
+      newUser.save().then((newDoc) => {
         res.json({ result: true, token: newDoc.token });
       });
     } else {
-  // Si l'utilisateur existe déjà...
-      res.json({ result: false, error: 'User already exists' });
+      // Si l'utilisateur existe déjà...
+      res.json({ result: false, error: "User already exists" });
     }
   });
 });
 
 // SIGNIN
 
-router.post('/signin', (req, res) => {
-  if (!checkBody(req.body, ['username', 'email', 'password'])) {
-    res.json({ result: false, error: 'Missing or empty fields' });
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
     return;
   }
 
   // Find avec $and qui permet de vérifier que le username ET l'email sont bons
 
-  User.findOne({ $and: [{ username: req.body.username }, { email: req.body.email }] }).then(data => {
+  User.findOne({ email: req.body.email }).then((data) => {
     if (!data) {
-      res.json({ result: false, error: 'User not found' });
+      res.json({ result: false, error: "User not found" });
     } else if (bcrypt.compareSync(req.body.password, data.password)) {
       res.json({ result: true, token: data.token });
     } else {
-      res.json({ result: false, error: 'Wrong password' });
+      res.json({ result: false, error: "Wrong password" });
     }
   });
 });
