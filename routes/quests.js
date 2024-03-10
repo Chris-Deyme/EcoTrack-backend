67ae09c72ddf41897aa231306e396374d36fa627
Viviane2111 <schommersv06@gmail.com>
2024-03-10 11:37:06 +0100

    ajout de commentaires sur la méthode MongoDB countDocuments

--- routes/quests.js
+++ routes/quests.js
@@ -9,18 +9,25 @@ router.get("/", (req, res) => {
 });
 
 router.get("/test", (req, res) => {
+   // countDocuments() >>> renvoi une promesse qui sera résolu ...
+   //... avec le nombre de document dans la collection, ...
+   // ... récupère le nombre de quest en BDD 
    Quest.countDocuments().then(count => {
       const randomQuest = Math.floor(Math.random() * count);
 
+      // opération d'agrégation dans MongoDB te permet de trier, ...
+      //... filtrer, et combiner des documents dans une collection
       Quest.aggregate([
+         // pour sauter un nombre aléatoire de quest dans la collection
          { $skip: randomQuest },
+         // donne le nombre de documents retenus (1)
          { $limit: 1 }
       ]).then(result => {
          res.json({ result: true, quest: result });
-      }).catch(err => {
+      }).catch(err => { // "capture" de l'erreur de .agrégate([{$skip, $limit}])
          res.status(500).json({ error: err.message });
       });
-   }).catch(err => {
+   }).catch(err => { // "capture" de l'erreur de .countDocuments()
       res.status(500).json({ error: err.message });
    });
 });
