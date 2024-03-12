75f5a701a26f686d81ee843f1fc7275b7fec9d52
Viviane2111 <schommersv06@gmail.com>
2024-03-12 17:12:05 +0100

    created route for searching action

--- routes/activities.js
+++ routes/activities.js
@@ -13,4 +13,18 @@ router.get("/showActivity/:category", (req, res) => {
   });
 });
 
+router.get("/activityName/:name", (req, res) => {
+  const nameRegex = new RegExp(req.params.name.split(" ").join("|"), "i");
+  Activity.find({ name: nameRegex })
+  // Activity.find({ name: req.params.name })
+  .then((data) => {
+    console.log("coucou ", data);
+    if (data) {
+      res.json({ result: true, activities: data });
+    } else {
+      res.json({ result: false, error: "Planetovore ! Trop nul." });
+    }
+  });
+})
+
 module.exports = router;
